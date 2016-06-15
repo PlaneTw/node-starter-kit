faae5197d0cb636171751d99415a605696dc496a
Plane <planetw.git@gmail.com>
2016-06-15 14:48:54 +0800

    Use pug template

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 import http from 'http';
 import express from 'express';
+import path from 'path'
 
 const app = express();
 const port = process.env.PORT || 3000;
+const viewPath = path.join(__dirname, 'views');
+
+app.set('view engine', 'pug');
+app.set('views', viewPath);
 
 app.get('/', (req, res) => {
     res.type('text/plain');
     res.send('Hello world!');
 });
 
-app.get('/user/:name', (req, res) => {
+app.get('/hi/:name', (req, res) => {
     let name = req.params.name
     res.type('text/plain');
     res.send('Hi ' + name + '!');
 })
+
+app.get('/hello/:name', (req, res) => {
+    let name = req.params.name
+    res.render("hello", {
+        name: name
+    });
+});
 
 app.listen(port, () => {
     console.log("Node app is running at localhost : " + port );
